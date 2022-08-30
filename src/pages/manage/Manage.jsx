45a4efc665abadc08007e68495211d1b81a57179
Omar Mirza <omirza@sabanciuniv.edu>
2022-08-30 17:59:05 +0300

    updates

--- src/pages/manage/Manage.jsx
+++ src/pages/manage/Manage.jsx
@@ -1,14 +1,40 @@
-import Sidebar from "../../components/sidebar/Sidebar";
 import "./manage.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import server from "../../components/widget/server.png";
 
 const Manage = () => {
-    return (
-        <div className="manage">
-            <Sidebar />
-            <div className="container">
-            </div>
+  return (
+    <div className="manage">
+      <Sidebar />
+      <div className="manageContainer">
+        <div className="top">
+          {/* <h1>{title}</h1> */}
         </div>
-    );
+        <div className="bottom">
+          <div className="left">
+            <img
+              src={server}
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+              </div>
+
+              {/* {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label>{input.label}</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              ))} */}
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Manage;
---
+++ src/service/service.js
@@ -0,0 +1,32 @@
+import http from "./http-common";
+const getAll = () => {
+  return http.get("/");
+};
+const get = ip => {
+  return http.get(`/${ip}`);
+};
+const create = data => {
+  return http.post("/", data);
+};
+const update = (ip, data) => {
+  return http.put(`/${ip}`, data);
+};
+const remove = ip => {
+  return http.delete(`/${ip}`);
+};
+// const removeAll = () => {
+//   return http.delete(`/`);
+// };
+// const findByTitle = title => {
+//   return http.get(`/?title=${title}`);
+// };
+const MachineService = {
+  getAll,
+  get,
+  create,
+  update,
+  remove
+//   removeAll,
+//   findByTitle
+};
+export default MachineService;
